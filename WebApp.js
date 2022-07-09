d856bcce0f789a1ce17a2ee4d4487e73f3c32d05
Aleksei Zhuvikin <zhuvikin@gmail.com>
2022-07-10 00:41:51 +0300

    Fix RN compilation

--- WebApp.js
+++ WebApp.js
@@ -2,9 +2,6 @@ import React from "react";
 import { webViewRender } from "react-native-react-bridge/lib/web";
 import App from './src/App';
 
-import "./example.css";
-import image from "./logo.png";
-
 const Root = () => {
   return (
     <App />
