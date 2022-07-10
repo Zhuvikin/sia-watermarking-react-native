73c6dc81bcef376104f171c92a7863b2e3062c3e
Aleksei Zhuvikin <zhuvikin@gmail.com>
2022-07-10 11:13:20 +0300

    Clean up WebApp.tsx

--- WebApp.tsx
+++ WebApp.tsx
@@ -2,10 +2,4 @@ import React from "react";
 import { webViewRender } from "react-native-react-bridge/lib/web";
 import App from './src/App';
 
-const Root = () => {
-  return (
-    <App />
-  );
-};
-
-export default webViewRender(<Root />);
+export default webViewRender(<App />);
