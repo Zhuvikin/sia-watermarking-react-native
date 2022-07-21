daf930e9167b505023dc74bb777ffffd0133a5bc
Aleksei Zhuvikin <zhuvikin@gmail.com>
2022-07-21 16:37:07 +0300

    load content lazily

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, {Suspense} from 'react';
 import './App.css';
-import { ImageMagick, initImageMagick } from "./lib/imagemagick";
-import { GSL, initGSL } from "./lib/gsl";
 import {Header} from "./header/Header";
-import {Content} from "./content/Content";
 
-function App() {
-  const [gslModule, setGSLModule] : [GSL | undefined, React.Dispatch<React.SetStateAction<GSL | undefined>>] = useState();
-  const [imageMagickModule, setImageMagickModule] : [ImageMagick | undefined, React.Dispatch<React.SetStateAction<ImageMagick | undefined>>] = useState();
+const Content = React.lazy(() => import('./content/Content'));
 
-  useEffect(
-    () => {
-      initImageMagick().then((Module : ImageMagick) => {
-        setImageMagickModule(() => Module);
-      });
-      initGSL().then((Module : GSL) => {
-        setGSLModule(() => Module);
-      });
-    },
-    []
-  );
-
-  if (!gslModule || !imageMagickModule) {
-    return (
-      <div className="App">Loading webassembly...</div>
-    )
-  }
-
-  return (
-    <div className="App">
-      <Header />
-      <Content gslModule={gslModule} imageMagickModule={imageMagickModule} />
-    </div>
-  );
-}
-
-export default App;
+export default () => <div className="App">
+    <Suspense fallback={<div>Loading...</div>}>
+        <Header/>
+        <Content/>
+    </Suspense>
+</div>;
--- src/content/Content.tsx
+++ src/content/Content.tsx
@@ -1,20 +1,38 @@
-import React from "react";
-import {GSL} from "../lib/gsl";
-import {ImageMagick} from "../lib/imagemagick";
+import React, {useEffect, useState} from "react";
+import {GSL, initGSL} from "../lib/gsl";
+import {ImageMagick, initImageMagick} from "../lib/imagemagick";
 import {StepView} from "./StepView";
 import {GSLTest} from "./GSLTest/GSLTest";
 import {ImageLoad} from "./ImageLoad/ImageLoad";
 
-type ContentProps = {
-    gslModule: GSL
-    imageMagickModule: ImageMagick
-};
+export default () => {
+    const [gslModule, setGSLModule] : [GSL | undefined, React.Dispatch<React.SetStateAction<GSL | undefined>>] = useState();
+    const [imageMagickModule, setImageMagickModule] : [ImageMagick | undefined, React.Dispatch<React.SetStateAction<ImageMagick | undefined>>] = useState();
 
-export const Content = ({gslModule, imageMagickModule}: ContentProps) => <div className="content">
-    <StepView title="GNU Scientific Library">
-        <GSLTest gslModule={gslModule}/>
-    </StepView>
-    <StepView title="Load Image">
-        <ImageLoad imageMagickModule={imageMagickModule}/>
-    </StepView>
-</div>;
+    useEffect(
+        () => {
+            initImageMagick().then((Module : ImageMagick) => {
+                setImageMagickModule(() => Module);
+            });
+            initGSL().then((Module : GSL) => {
+                setGSLModule(() => Module);
+            });
+        },
+        []
+    );
+
+    if (!gslModule || !imageMagickModule) {
+        return (
+            <div className="App">Loading webassembly...</div>
+        )
+    }
+
+    return <div className="content">
+        <StepView title="GNU Scientific Library">
+            <GSLTest gslModule={gslModule}/>
+        </StepView>
+        <StepView title="Load Image">
+            <ImageLoad imageMagickModule={imageMagickModule}/>
+        </StepView>
+    </div>;
+};
